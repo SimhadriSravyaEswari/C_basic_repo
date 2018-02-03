3a8b49c3b82ee8fa47dff81aa1423a65d2504af9
Sravya Eswari <simhadrisravyaeswari@gmail.com>
2018-02-02 23:08:39 -0800

    Simple Interest

--- C_03_SimInt/C_03_SimInt.c
+++ C_03_SimInt/C_03_SimInt.c
@@ -1,14 +1,14 @@
 # include<stdio.h>
 int main()
 {
-    float p,t,r,SI;
+    float principal,time,rate,simint;
     printf("Enter principal amount ");
-    scanf("%f",&p);
+    scanf("%f",&principal);
     printf("Enter time in years ");
-    scanf("%f",&t);
+    scanf("%f",&time);
     printf("Enter rate");
-    scanf("%f",&r);
-    SI = (p*t*r)/100;
-    printf("\nThe simple interest is %f", SI); 
+    scanf("%f",&rate);
+    simint = (principal*time*rate)/100;
+    printf("\nThe simple interest is %f", simint); 
     return 0;
 }
